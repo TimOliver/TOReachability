944fd3ecd722d595adbdaa1ab9fc831fcb4da4ff
Tim Oliver <me@timoliver.com.au>
2019-03-07 01:18:22 +0900

    Removed empty delegate code

--- TOReachabilityExample/AppDelegate.m
+++ TOReachabilityExample/AppDelegate.m
@@ -8,17 +8,9 @@
 
 #import "AppDelegate.h"
 
-
-@interface AppDelegate ()
-
-@end
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-
-
     return YES;
 }
 
